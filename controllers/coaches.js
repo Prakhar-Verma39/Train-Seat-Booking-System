d2d23eaff3faaa386859f08d76d4282ebd6cbda0
Prakhar-Verma39 <103757447+Prakhar-Verma39@users.noreply.github.com>
2023-08-14 02:17:10 +0530

    total empty seats problem resolved

--- controllers/coaches.js
+++ controllers/coaches.js
@@ -72,87 +72,97 @@ module.exports.updateCoach = async (req, res) => {
 // "update" tickets on first row only
 const { coach, id } = await Coach.findById(req.params.id);
 let { required_seats } = req.body;
-let max = 0
+let total_empty_seats = 0
 
-while(required_seats > 0)
-{
-    let max_row = null;
-    let max_row_number = 0;
+// to get total empty seats
+for (let row_id of coach) {
+    let row = await Row.findById(row_id)
+    total_empty_seats += row.emptySeats;
+}
 
-    // Iterate through coach rows
-    for(let row_number=0;row_number < coach.length; row_number++)
+if(total_empty_seats >= required_seats){
+    let max = 0
+
+    while(required_seats > 0)
     {
-        let row_id = coach[row_number];
-        let row = await Row.findById(row_id)
-        
-        // Check if the row is available and has enough seats
-        if(row.isAvailable && row.emptySeats >= required_seats)
+        let max_row = null;
+        let max_row_number = 0;
+
+        // Iterate through coach rows
+        for(let row_number=0;row_number < coach.length; row_number++)
         {
-            assign_seat(row, row_number, required_seats) // Assign seats
-            required_seats = 0
-        }
-        else if(row.isAvailable && row.emptySeats > max)
-        {
-            max = row.emptySeats
-            max_row = row
-            max_row_number = row_number
-        }
-
-    }
-
-    // Assign seats on max row with remaining required seats
-    while(required_seats > 0 && max > 0){
-        assign_seat(max_row, max_row_number, Math.min(required_seats, max))
-        required_seats -= Math.min(required_seats, max)
-
-        // If more seats required, update max_row and max
-        if(required_seats > 0){
-            max_row_number = update_max_row(max_row_number)
-            max_row = await Row.findById(coach[max_row_number])
-            max = max_row.emptySeats
-        }
-    }
-
-    // Function to assign seats to a row
-    async function assign_seat(row, row_number, required_seats){
-        // to make required number of tickets and provide them seat numbers
-        const tickets = Array.from({ length: parseInt(required_seats) }, (_, i) => new Ticket({ seat_number: i + 1 + row.seats.length + row_number*7}));
-        await Ticket.insertMany(tickets);
-
-        // to add new tickets in seats array, update ramaining empty seats in one row 
-        // and if all empty seats are booked then row becomes unavailable
-        await Row.findByIdAndUpdate(coach[row_number], {
-            $push: { seats: { $each: tickets } },
-            $inc: { emptySeats: -tickets.length },
-            $set: { isAvailable: row.emptySeats !== tickets.length } 
-        }, { new: true });
-    }
-
-    // Function to update max_row based on availability and empty seats
-    function update_max_row(max_row_number){
-        if(max_row_number + 1 < coach.length && max_row_number >= 0){
-            let before_row = Row.findById(coach[max_row_number - 1])
-            let after_row = Row.findById(coach[max_row_number + 1])
-            if(after_row.isAvailable){
-                if(before_row.isAvailable && before_row.emptySeats >= after_row.emptySeats){
-                    max_row_number -= 1}
-                else{
-                    max_row_number += 1}
+            let row_id = coach[row_number];
+            let row = await Row.findById(row_id)
+            
+            // Check if the row is available and has enough seats
+            if(row.isAvailable && row.emptySeats >= required_seats)
+            {
+                assign_seat(row, row_number, required_seats) // Assign seats
+                required_seats = 0
             }
-            else if(before_row.isAvailable){
+            else if(row.isAvailable && row.emptySeats > max)
+            {
+                max = row.emptySeats
+                max_row = row
+                max_row_number = row_number
+            }
+
+        }
+
+        // Assign seats on max row with remaining required seats
+        while(required_seats > 0 && max > 0){
+            assign_seat(max_row, max_row_number, Math.min(required_seats, max))
+            required_seats -= Math.min(required_seats, max)
+
+            // If more seats required, update max_row and max
+            if(required_seats > 0){
+                max_row_number = update_max_row(max_row_number)
+                max_row = await Row.findById(coach[max_row_number])
+                max = max_row.emptySeats
+            }
+        }
+
+        // Function to assign seats to a row
+        async function assign_seat(row, row_number, required_seats){
+            // to make required number of tickets and provide them seat numbers
+            const tickets = Array.from({ length: parseInt(required_seats) }, (_, i) => new Ticket({ seat_number: i + 1 + row.seats.length + row_number*7}));
+            await Ticket.insertMany(tickets);
+
+            // to add new tickets in seats array, update ramaining empty seats in one row 
+            // and if all empty seats are booked then row becomes unavailable
+            await Row.findByIdAndUpdate(coach[row_number], {
+                $push: { seats: { $each: tickets } },
+                $inc: { emptySeats: -tickets.length },
+                $set: { isAvailable: row.emptySeats !== tickets.length } 
+            }, { new: true });
+        }
+
+        // Function to update max_row based on availability and empty seats
+        function update_max_row(max_row_number){
+            if(max_row_number + 1 < coach.length && max_row_number >= 0){
+                let before_row = Row.findById(coach[max_row_number - 1])
+                let after_row = Row.findById(coach[max_row_number + 1])
+                if(after_row.isAvailable){
+                    if(before_row.isAvailable && before_row.emptySeats >= after_row.emptySeats){
+                        max_row_number -= 1}
+                    else{
+                        max_row_number += 1}
+                }
+                else if(before_row.isAvailable){
+                    max_row_number -= 1
+                }
+                else{
+                    max_row_number = (max_row_number + 1) % coach.length
+                }
+            }
+            else if(max_row_number + 1 >= coach.length){
                 max_row_number -= 1
             }
             else{
-                max_row_number = (max_row_number + 1) % coach.length
+                max_row_number += 1
             }
+            return max_row_number
         }
-        else if(max_row_number + 1 >= coach.length){
-            max_row_number -= 1
-        }
-        else{
-            max_row_number += 1
-        }
-        return max_row_number
     }
 }
     res.redirect(`/coaches/${id}`)
